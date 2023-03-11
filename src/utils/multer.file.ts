f54f7a8d30dffb290d84b53ec85b442b955ccc57
Enio de Oliveira <enio1303@gmail.com>
2023-03-11 13:50:39 -0300

    multer consolelog

--- src/utils/multer.file.ts
+++ src/utils/multer.file.ts
@@ -1,6 +1,6 @@
 import multer from 'multer';
 
-
+console.log(`>>> ${process.cwd()}/uploads/`)
 // Configuração de armazenamento
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
